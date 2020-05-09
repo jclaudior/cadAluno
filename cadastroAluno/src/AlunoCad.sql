184a5305370ae00f6fa3e8f4f9c1537147eb1605
jclaudior <joaoclaudiodiva@gmail.com>
2020-05-09 12:36:33 -0300

    sql atualizado

--- cadastroAluno/src/AlunoCad.sql
+++ cadastroAluno/src/AlunoCad.sql
@@ -34,6 +34,10 @@ CREATE TABLE `cad_aluno` (
   UNIQUE KEY `ALUNO_CPF` (`ALUNO_CPF`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
 
+/*Data for the table `cad_aluno` */
+
+insert  into `cad_aluno`(`ALUNO_ID`,`ALUNO_NOME`,`ALUNO_DT_NASC`,`ALUNO_CPF`,`ALUNO_EMAIL`,`ALUNO_END`,`ALUNO_MUNIC`,`ALUNO_UF`,`ALUNO_CEL`) values (13,'JOAO CLAUDIO RIBEIRO','1997-07-12','22222222222','JOAOCLAUDIODIVA@GMAIL.COM','RUA DA MOCA 1736','SP','SP',11111111116),(18,'MARIA DAS NEVES','1998-09-18','55555555555','MARIA@GMAIL.COM','AV SAPOPEMBA 13304','SAO PAULO','SP',44444444444);
+
 /*Table structure for table `cad_campus` */
 
 DROP TABLE IF EXISTS `cad_campus`;
@@ -43,6 +47,10 @@ CREATE TABLE `cad_campus` (
   `CAMPUS_NOME` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`CAMPUS_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `cad_campus` */
+
+insert  into `cad_campus`(`CAMPUS_ID`,`CAMPUS_NOME`) values (1,'TATUAPE'),(2,'PINHEIROS');
 
 /*Table structure for table `cad_curso` */
 
@@ -54,6 +62,10 @@ CREATE TABLE `cad_curso` (
   PRIMARY KEY (`CURSO_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
+/*Data for the table `cad_curso` */
+
+insert  into `cad_curso`(`CURSO_ID`,`CURSO_DESC`) values (1,'ANALISE E DESENVOLVIMENTO DE SISTEMA'),(2,'CIENCIAS DA COMPUTACAO'),(3,'ENGENHARIA DE SOFTWARE'),(4,'BANCO DE DADOS'),(5,'REDES DE COMPUTADORES');
+
 /*Table structure for table `cad_disciplina` */
 
 DROP TABLE IF EXISTS `cad_disciplina`;
@@ -63,6 +75,10 @@ CREATE TABLE `cad_disciplina` (
   `DISCIPLINA_DESC` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`DISCIPLINA_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `cad_disciplina` */
+
+insert  into `cad_disciplina`(`DISCIPLINA_ID`,`DISCIPLINA_DESC`) values (1,'MODELAGEM DE DADOS'),(2,'ESTRUTURA DE DADOS'),(3,'TECNICAS DE PROGRAMACAO'),(4,'BANCO DE DADOS'),(5,'LOGICA DE PROGRAMACAO'),(6,'REDES DE COMPUTADORES'),(7,'ANALISE DE PROJETO');
 
 /*Table structure for table `cad_disciplina_curso` */
 
@@ -76,6 +92,10 @@ CREATE TABLE `cad_disciplina_curso` (
   CONSTRAINT `FK_CAD_DISCIPLINA_CURSO_CURSO` FOREIGN KEY (`CURSO_ID`) REFERENCES `cad_curso` (`CURSO_ID`),
   CONSTRAINT `FK_CAD_DISCIPLINA_CURSO_DISCIPLINA` FOREIGN KEY (`DISCIPLINA_ID`) REFERENCES `cad_disciplina` (`DISCIPLINA_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `cad_disciplina_curso` */
+
+insert  into `cad_disciplina_curso`(`DISCIPLINA_ID`,`CURSO_ID`) values (1,1),(1,2),(1,3),(1,4),(1,5),(2,1),(2,2),(2,3),(2,4),(2,5),(3,1),(3,2),(3,3),(3,4),(3,5),(4,1),(4,2),(4,3),(4,4),(4,5),(5,1),(5,2),(5,3),(5,4),(5,5),(6,1),(6,2),(6,3),(6,4),(6,5),(7,1),(7,2),(7,3),(7,4),(7,5);
 
 /*Table structure for table `cad_matricula` */
 
@@ -98,6 +118,10 @@ CREATE TABLE `cad_matricula` (
   CONSTRAINT `FK_CAD_MATRICULA_CAD_PERI0DO` FOREIGN KEY (`PERIODO_ID`) REFERENCES `cad_periodo` (`PERIODO_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22110130 DEFAULT CHARSET=utf8mb4;
 
+/*Data for the table `cad_matricula` */
+
+insert  into `cad_matricula`(`MATRICULA_ID`,`ALUNO_ID`,`CURSO_ID`,`CAMPUS_ID`,`PERIODO_ID`) values (22110127,13,1,1,3),(22110128,18,1,1,3);
+
 /*Table structure for table `cad_periodo` */
 
 DROP TABLE IF EXISTS `cad_periodo`;
@@ -107,6 +131,10 @@ CREATE TABLE `cad_periodo` (
   `PERIODO_DESC` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`PERIODO_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `cad_periodo` */
+
+insert  into `cad_periodo`(`PERIODO_ID`,`PERIODO_DESC`) values (1,'MATUTINO'),(2,'VESPERTINO'),(3,'NOTURNO');
 
 /*Table structure for table `cad_ponderado` */
 
@@ -123,6 +151,10 @@ CREATE TABLE `cad_ponderado` (
   CONSTRAINT `FK_CAD_PONDERADO_CAD_DISCIPLINA` FOREIGN KEY (`DISCIPLINA_ID`) REFERENCES `cad_disciplina` (`DISCIPLINA_ID`),
   CONSTRAINT `FK_CAD_PONDERADO_CAD_MATRICULA` FOREIGN KEY (`MATRICULA_ID`) REFERENCES `cad_matricula` (`MATRICULA_ID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `cad_ponderado` */
+
+insert  into `cad_ponderado`(`DISCIPLINA_ID`,`MATRICULA_ID`,`PONDERADO_NOTA`,`PONDERADO_FALTAS`,`PONDERADO_SEMEST`) values (1,22110127,'07.9',2,'2019-1'),(1,22110128,'03.0',3,'2019-1'),(2,22110127,'07.9',2,'2019-1'),(2,22110128,'01.5',0,'2019-1'),(3,22110127,'09.0',3,'2019-1'),(3,22110128,'03.0',4,'2019-1'),(4,22110127,'03.0',2,'2019-1'),(5,22110127,'07.9',2,'2019-1'),(6,22110127,'07.9',2,'2019-1'),(7,22110127,'03.5',1,'2019-1');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
